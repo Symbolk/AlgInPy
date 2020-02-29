88048486f39b56e8a9090ca3e346c6885d56500d
Symbolk <symbolk@163.com>
2020-02-29 10:49:46 +0800

    [20200229] Rename.

--- string/151ReverseWords.py
+++ string/151ReverseWords.py
@@ -1,5 +1,5 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
         # return ' '.join(s.split()[::-1])
-        # return ' '.join(re.findall('[^ ]+', s)[::-1])
+        return ' '.join(re.findall('[^ ]+', s)[::-1])
         # return ' '.join([i for i in s.split() if i][::-1]).strip()
