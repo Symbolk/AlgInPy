9af14c4e43392ccf8a3d30dddc1d8e037b8deabf
Symbolk <symbolk@163.com>
2020-03-27 19:22:27 +0800

    [20200327] IO templates for ACM.

--- test/gcd_lcm.py
+++ test/gcd_lcm.py
@@ -6,14 +6,14 @@ def gcd(a, b):
 def gcd2(a, b):
     return a if b == 0 else gcd(b, a % b)
 
+
 # Least Common Multiple
 def lcm(a, b):
     return int(a / gcd(a, b) * b)
 
 
-raw = input()
-n, m = raw.split(' ')
-n, m = int(n), int(m)
-print(gcd(n, m))
-print(gcd2(n, m))
+# input two ints
+n, m = map(int, input().split())
+print(gcd(n, m), end=", ")
+print(gcd2(n, m), end=',')
 print(lcm(n, m))
