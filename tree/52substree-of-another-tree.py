5bcb0d3eef0a0c62410c997a66cf3f3bb9007070
Symbolk <symbolk@163.com>
2020-05-07 23:31:30 +0800

    [20200507] Subtree checking.

--- tree/52substree-of-another-tree.py
+++ tree/52substree-of-another-tree.py
@@ -7,15 +7,15 @@
 
 class Solution:
     # convert and compare: !special symbols are used to represent same val but diff pos
+    # to generate unique preorder seq for each tree
     def isSubtree(self, s: TreeNode, t: TreeNode) -> bool:
         def preorder(root, isleft):
             if not root:
                 return 'lnull' if isleft else 'rnull'
             res = ''
-            if root:
-                res = res + "#" + str(root.val)
-                res = res + "#" + preorder(root.left, True)
-                res = res + '#' + preorder(root.right, False)
+            res = res + "#" + str(root.val)
+            res = res + "#" + preorder(root.left, True)
+            res = res + '#' + preorder(root.right, False)
             return res
 
         ss = preorder(s, False)
@@ -34,8 +34,10 @@ class Solution:
         return self.isSame(s, t) or self.isSubtree(s.left, t) or self.isSubtree(s.right, t)
 
     def isSame(self, s, t):
+        # None tree is the same with None tree
         if not s and not t:
             return True
+        # None tree is not the same with non-None tree
         if not s or not t:
             return False
         return s.val == t.val and self.isSame(s.left, t.left) and self.isSame(s.right, t.right)
