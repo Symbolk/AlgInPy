728342737fdad54423b543f1c38f58748e38702b
Symbolk <symbolk@163.com>
2020-05-29 19:31:27 +0800

    [20200529] Simple DP.

--- DP/1716the-masseuse-lcci.py
+++ DP/1716the-masseuse-lcci.py
@@ -1,6 +1,36 @@
 class Solution:
-    # total space: 2N O(n), O(1)
+    # same with house-robber
+    # naive dp: O(n), O(n)
     def massage(self, nums: List[int]) -> int:
+        N = len(nums)
+        if N == 0:
+            return 0
+        if N == 1:
+            return nums[0]
+        dp = [[0] * 2 for _ in range(N)]
+        dp[0][0] = 0
+        dp[0][1] = nums[0]
+        for i in range(1, N):
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1])
+            dp[i][1] = dp[i - 1][0] + nums[i]
+        return max(dp[-1][0], dp[-1][1])
+
+    # O(n), O(n)
+    def massage1(self, nums: List[int]) -> int:
+        N = len(nums)
+        if N == 0:
+            return 0
+        if N == 1:
+            return nums[0]
+        dp = [0] * N
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        for i in range(2, N):
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
+        return dp[-1]
+
+    # total space: 2N O(n), O(1)
+    def massage2(self, nums: List[int]) -> int:
         N = len(nums)
         if N == 0:
             return 0
--- DP/198HouseRobber.py
+++ DP/198HouseRobber.py
@@ -2,6 +2,7 @@ from typing import List
 
 
 class Solution:
+    # O(n), O(n)
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
         if n == 0:
@@ -12,9 +13,28 @@ class Solution:
         dp = [[0] * 2 for _ in range(n)]
         for i in range(n):
             if i == 0:
-            dp[i][1] = dp[i - 1][0] + nums[i]
+                dp[i][0] = 0
+                dp[i][1] = nums[i]
+            else:  # i >= 1
+                dp[i][0] = max(dp[i - 1][0], dp[i - 1][1])
+                dp[i][1] = dp[i - 1][0] + nums[i]
         return max(dp[-1][0], dp[-1][1])
 
+    # O(n), O(n)
+    def rob1(self, nums: List[int]) -> int:
+        N = len(nums)
+        if N == 0:
+            return 0
+        if N == 1:
+            return nums[0]
+        dp = [0] * N
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        for i in range(2, N):
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
+        return dp[n - 1]
+
+    # O(n), O(n)
     def rob2(self, nums: List[int]) -> int:
         n = len(nums)
         if n == 0:
@@ -23,8 +43,18 @@ class Solution:
         dp[0] = 0
         dp[1] = nums[0]
         for i in range(2, n + 1):
-            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i - 1]);
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i - 1])
         return dp[n]
+
+    # O(n), O(1)
+    def rob3(self, nums: List[int]) -> int:
+        # i-2, i-1
+        a, b = 0, 0
+        for n in nums:
+            c = max(b, a + n)
+            a = b
+            b = c
+        return b
 
 
 if __name__ == "__main__":
