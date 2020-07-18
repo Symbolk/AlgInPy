b9c484c73c64913fe542a2cde2d0bdb02c0b477b
Symbolk <symbolk@163.com>
2020-07-18 21:27:15 +0800

    [20200718] Approximation algorithm revisited.

--- graph/best-position-for-a-service-centre.py
+++ graph/best-position-for-a-service-centre.py
@@ -1,6 +1,8 @@
 from typing import List
 
-
+# Geometric Median
+# There isn’t any defined correct algorithm for finding the Geometric Median
+# Here we are approximating a solution
 class Solution:
     def disSum(self, x, y, pos):
         dis_sum = 0
@@ -12,16 +14,20 @@ class Solution:
         return dis_sum
 
     def getMinDistSum(self, positions: List[List[int]]) -> float:
+        # current temp median (ans)
         x, y = 0, 0
         N = len(positions)
         for p in positions:
             x += p[0]
             y += p[1]
+        # get the Centroid
         x /= N
         y /= N
 
+        # current temp min distance sum
         min_dis = self.disSum(x, y, positions)
 
+        # iterate over the given input points
         k = 0
         while k < N:
             for i in range(N):
@@ -34,21 +40,25 @@ class Solution:
                     x, y = xx, yy
             k += 1
 
-        test_dis = 1000
-        test_p = [[-1.0, 0.0], [0.0, 1.0], [1.0, 0.0], [0.0, -1.0]]
-        while test_dis > 10 ** -5:
+        # learning rate/step
+        step = 1000
+        move = [[-1.0, 0.0], [0.0, 1.0], [1.0, 0.0], [0.0, -1.0]]
+        # terminate when lower_limit exceeds the test_distance
+        while step > 10 ** -5:
             flag = 0
             for i in range(4):
-                xx = x + test_dis * test_p[i][0]
-                yy = y + test_dis * test_p[i][1]
+                xx = x + step * move[i][0]
+                yy = y + step * move[i][1]
                 dd = self.disSum(xx, yy, positions)
                 if dd < min_dis:
                     min_dis = dd
                     x, y = xx, yy
+                    # find one then break to move from new (x,y)
                     flag = 1
                     break
+            # else binary the step
             if flag == 0:
-                test_dis /= 2
+                step /= 2
         return min_dis
 
 
